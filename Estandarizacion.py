02f24f1fa3cda733ecacd36d09bc7cd481405a33
100451105 <100451105@alumnos.uc3m.es>
2023-12-23 15:57:55 +0100

    Estandarización de tipo de actividad y web scrapper de información de asignaturas

--- Estandarizacion.py
+++ Estandarizacion.py
@@ -1,4 +1,5 @@
 import pandas
+import re
 
 
 # Función para expandir rangos en la columna 'Semanas'. 
@@ -28,29 +29,60 @@ if __name__ == "__main__":
     
     # Primer preprocesado: depuramos el valor de la columna 'Semana'
     tabla['Semana_expandida'] = tabla['Semana'].apply(expandir_rangos)
-    print(tabla.loc[tabla['Codigo_asignatura'] == 15974])
     tabla = tabla.explode('Semana_expandida', ignore_index=True)
-    print(tabla)
     tabla.drop('Semana', axis=1, inplace=True)
     tabla.reset_index(drop=True, inplace=True)
     tabla.rename(columns={'Semana_expandida': 'Semana'}, inplace=True)
     tabla = tabla[['Codigo_asignatura', 'Semana', 'Actividad']]
     tabla.reset_index(drop=True, inplace=True)
-    # Nota: semana sigue siendo una lista. Para convertirlo a un string, usar:
-    print(tabla)
-    # Comprobación de rango d-d: buscar valores del código de asignatura 13881
-    print(tabla.loc[tabla['Codigo_asignatura'] == 13881])
     # Por último, eliminar filas que contengan valores nulos en la columna 'Semana'
     tabla.dropna(axis=0, inplace=True)
     tabla.reset_index(drop=True, inplace=True)
+    print("Cleaned the Semana column")
 
-    # Segundo preprocesado: (pendiente) Asignar a cada código de asignatura el nombre de la asignatura correspondiente
+    # Segundo preprocesado: Asignar a cada código de asignatura distintas características (nombre, créditos, etc.)
+    asignaturas = pandas.read_csv("Dataset_Traducido/Asignaturas.csv")
+    # Juntamos la tabla con la de asignaturas en base al código de asignatura
+    asignaturas.rename(columns={'Código': 'Codigo_asignatura'}, inplace=True)
+    asignaturas.reset_index(drop=True, inplace=True)
+    print("Read the Asignaturas table.")
+    tabla = tabla.merge(asignaturas, on='Codigo_asignatura')
+    tabla = tabla[['Codigo_asignatura', 'Nombre', 'Créditos', 'Curso', 'Cuatrimestre', 'Tipo de asignatura', 'Semana', 'Actividad']]
+    tabla.reset_index(drop=True, inplace=True)
+    print("Added the columns from the Asignaturas table to the main table.")
 
-    # Tercer preprocesado: (pendiente) Definir tipo de Actividad (Ejercicio, Práctica, Examen, Laboratorio, Otros)
+    # Tercer preprocesado: Definir tipo de Actividad (Ejercicio, Práctica, Examen, Laboratorio, Otros)
+    # Buscamos dentro de la columna 'Actividad' distintos patrones que nos permitan clasificar las actividades:
+        # - Ejercicio: Contiene la palabra 'Ejercicio' o variantes (Ejercicios, Ejercicios de clase, etc.)
+        # - Práctica: Contiene la palabra 'Práctica' o variantes (Prácticas, P1,  etc.)
+        # - Examen: Contiene la palabra 'Examen' o variantes (Examen, Examen final, etc.)
+        # - Laboratorio: Contiene la palabra 'Laboratorio' o variantes (Laboratorio, Laboratorio de prácticas, etc.)
+        # - Otros: No contiene ninguna de las palabras anteriores
+    # Para ello, primero convertimos todas las actividades a minúsculas y luego aplicamos patrones con expresiones regulares
+    tabla_copia = tabla.copy()
+    tabla_copia['Actividad'] = tabla_copia['Actividad'].str.lower()
+    # Ejercicio
+    patron = r'.*ejercicio.*'
+    tabla_copia['Actividad'] = tabla_copia['Actividad'].apply(lambda x: 'Ejercicio' if re.match(patron, x) else x)
+    # Práctica
+    patron = r'.*práctica.*|.*p\d+.*|.*prácticas.*|.*práctico.*|.*proyecto.*'
+    tabla_copia['Actividad'] = tabla_copia['Actividad'].apply(lambda x: 'Práctica' if re.match(patron, x) else x)
+    # Laboratorio
+    patron = r'.*laboratorio.*|.*lab.*'
+    tabla_copia['Actividad'] = tabla_copia['Actividad'].apply(lambda x: 'Laboratorio' if re.match(patron, x) else x)
+    # Examen
+    patron = r'.*examen.*|.*test.*|.*prueba.*|.*control.*'
+    tabla_copia['Actividad'] = tabla_copia['Actividad'].apply(lambda x: 'Examen' if re.match(patron, x) else x)
+    # Otros
+    tabla_copia['Actividad'] = tabla_copia['Actividad'].apply(lambda x: 'Otros' if x not in ['Ejercicio', 'Práctica', 'Laboratorio', 'Examen'] else x)
+    # Creamos una columna nueva en la tabla original con los valores de la tabla copia
+    tabla['Tipo de actividad'] = tabla_copia['Actividad']
+    tabla.reset_index(drop=True, inplace=True)
+    print("Categorized the activities.")
     
-
     # Exportar las tablas a distintos archivos csv
     csv_path = "Dataset_Traducido/Dataset_limpio.csv"
     tabla.to_csv(csv_path, index=False)
+    print("Exported the table to the csv file: " + csv_path)
 
     # [0-9][0-9][0-9][0-9][0-9]-[a-zA-Z0-9]*,[a-zA-Z0-9]*
---
+++ Recolector_de_Web.py
@@ -0,0 +1,117 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import csv
+
+# Función que recibe la url de una página web y devuelve el contenido de la página (formato html)
+
+def get_html(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        html_content = response.content.decode('utf-8')
+        soup = BeautifulSoup(html_content, 'html.parser')
+        return soup
+    else:
+        print("Error al obtener el contenido de la página. Código de error: " + str(response.status_code) + ".")
+        return None
+    
+
+# Todo el formateado del html que se encuentra se debe limpiar. Para ello, esta función realiza una limpieza de texto general
+def buscar_patron(texto,patron):
+    # Buscamos el patrón en la lista de textos filtrada
+    patron = re.compile(patron, re.UNICODE)
+    resultado = patron.search(texto)
+    if resultado:
+        return resultado.groups()
+    else:
+        return -1
+
+def format_text(texto, patron):
+    texto = texto.replace("\t", "")
+    # Dividir el texto por cada salto de línea
+    texto = texto.split("\n")
+    # Eliminar elementos vacíos de la lista
+    texto = list(filter(None, texto))
+    texto = list(filter(lambda x: x != " ", texto))
+    # Colocar todos los textos de vuelta en un string separados por \n
+    texto = "\n".join(texto)
+    if patron:
+        return buscar_patron(texto,patron)
+    else:
+        return texto
+    
+
+if __name__ == "__main__":
+    page = get_html("https://aplicaciones.uc3m.es/consultaHorarios/porPlan.htm?ano=2023&centro=2&plan=489&idioma=es")
+    
+    # Una vez guardada la pagina se realiza un análisis de qué etiquetas nos interesan:
+        # class="asignatura" --> Código de asignatura-Nombre,Créditos
+        # class="cuatr" --> Cuatrimestre
+        # class="tipoAsig" --> Tipo de asignatura (Obligatoria, Optativa, etc.)
+        # class="curso" --> Curso de la asignatura
+
+    # Comenzamos creando una lista con todas las etiquetas que tengan clase asignatura con el formato establecido (utilizamos expresiones regulares)
+    asignaturas = page.find_all("li", {"class": "asignatura"})
+    
+    # Creamos un diccionario para guardar los datos de las asignaturas
+    asignaturas_dict = {}
+    for asignatura in asignaturas:
+        # Descomponemos el texto para adquirir los textos que nos interesan (numero-nombre,créditos) utilizando expresiones regulares
+        texto = asignatura.text
+        # Limpiar el texto de saltos de línea y espacios
+        patron = r'(\d+)-(.+?), (\d+(\.\d+)?) ECTS'
+        texto = format_text(texto, patron)
+        if texto == -1:
+            continue
+        codigo = texto[0]
+        nombre = texto[1]
+        creditos = texto[2]
+        # 2a parte: Buscamos la correlación de dicha asignatura con sus respectivos cuatrimestres, tipo de asignatura y curso
+        curso = asignatura.find_previous("li", {"class": "curso"})
+        texto = curso.text
+        texto = format_text(texto, None)
+        # Aplicamos 3 patrones distintos para obtener los datos de cuatrimestre, tipo de asignatura y curso:
+        # Curso: 1er curso, 2o Curso, 3o Curso, 4o Curso, Curso
+        patron = r'(\d+)([a-z]{1,2}) Curso'
+        resultado = buscar_patron(texto, patron)
+        if resultado == -1:
+            patron = r'(\d+)([a-z]{1,2})Curso'
+            resultado = buscar_patron(texto, patron)
+            if resultado == -1:
+                patron = r'(Curso)'
+                resultado = buscar_patron(texto, patron)
+                curso_buscado = resultado[0]   
+            else:
+                curso_buscado = resultado[0]
+        else:
+            curso_buscado = resultado[0] + resultado[1]
+        # Tipo de asignatura: Obligatoria, Optativa, Cursos de Humanidades, Formación Básica
+        patron = r'(Obligatoria|Optativa|Cursos de Humanidades|Formación Básica)'
+        resultado = buscar_patron(texto, patron)
+        if resultado == -1:
+            tipo_asignatura = "No especificado"
+        else:
+            tipo_asignatura = resultado[0]
+        # Cuatrimestre: 1er cuatrimestre, 2o cuatrimestre
+        cuatrimestre = asignatura.find_previous("li", {"class": "cuatr"})
+        texto = cuatrimestre.text
+        texto = format_text(texto, None)
+        patron = r'(1erCuatrimestre|2o Cuatrimestre)'
+        resultado = buscar_patron(texto, patron)
+        if resultado == -1:
+            cuatrimestre = "No especificado"
+        else:
+            cuatrimestre = resultado[0]
+        # Quitar los espacios del cuatrimestre
+        cuatrimestre = cuatrimestre.replace(" ", "")
+        # Añadimos los datos al diccionario
+        asignaturas_dict[codigo] = [curso_buscado, tipo_asignatura, cuatrimestre, nombre, creditos]
+
+    # Una vez tenemos el diccionario con los datos de las asignaturas, procedemos a crear un archivo csv con los datos
+    # Para ello, utilizamos la librería csv
+    with open("Dataset_Traducido/Asignaturas.csv", "w", encoding="utf-8") as file:
+        csv.DictWriter(file, fieldnames=["Código", "Curso", "Tipo de asignatura", "Cuatrimestre", "Nombre", "Créditos"]).writeheader()
+        for codigo, datos in asignaturas_dict.items():
+            csv.DictWriter(file, fieldnames=["Código", "Curso", "Tipo de asignatura", "Cuatrimestre", "Nombre", "Créditos"]).writerow({"Código": codigo, "Curso": datos[0], "Tipo de asignatura": datos[1], "Cuatrimestre": datos[2], "Nombre": datos[3], "Créditos": datos[4]})
+    
+        
